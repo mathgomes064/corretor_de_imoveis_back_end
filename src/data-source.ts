10bcfc40c74f69d73d3f6ba94e4f082557a7d28b
Matheus_Gomes <mathgomes007@htomail.com>
2023-04-17 11:34:44 -0300

    tadabase url

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,12 +7,13 @@ require("dotenv").config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
+    url: process.env.DATABASE_URL,
+    // host: "localhost",
+    // port: 5432,
 
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database:  process.env.POSTGRES_DB,
+    // username: process.env.POSTGRES_USER,
+    // password: process.env.POSTGRES_PASSWORD,
+    // database:  process.env.POSTGRES_DB,
 
     synchronize: false,
     logging: true,
