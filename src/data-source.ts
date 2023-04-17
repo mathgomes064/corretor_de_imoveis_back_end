7fd6974176bae8f1204cc78e04a64bdc3e3dc826
Matheus_Gomes <mathgomes007@htomail.com>
2023-04-17 14:52:37 -0300

    ajuste nas migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,6 +3,10 @@ import { User } from "./entities/user.entity";
 import { error } from "console";
 import { Imovel } from "./entities/imovel.entity";
 import "dotenv/config"
+import {InitialMigration1681324964959} from "./migrations/1681324964959-initialMigration"
+import {GenerateTables1681324971971} from "./migrations/1681324971971-generateTables"
+import {Cascade1681565144418} from "./migrations/1681565144418-cascade"
+import {StatusChange1681557515781} from "./migrations/1681557515781-statusChange"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -12,5 +16,5 @@ export const AppDataSource = new DataSource({
     synchronize: false,
     logging: true,
     entities: [User, Imovel],
-    migrations: ["src/migrations/*.ts"],
+    migrations: [InitialMigration1681324964959, GenerateTables1681324971971, Cascade1681565144418, StatusChange1681557515781],
 })
